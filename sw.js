3cfaa73c4f0434b118df9977a06256869523b47b
tranhoadtp <tranhoadtp@gmail.com>
2026-09-24 15:15:20 +0700

    feat: add service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='bvcl-bedflow-v01';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
